1a508290b025378ab45b8900d02d47aaa5e11ab3
Kartikeya Chouhan <kartikey.chouhan8@gmail.com>
2024-09-01 08:31:01 +0000

    initial commit

---
+++ first_project/target/debug/deps/first_project-c9e55de654c25ad7.d
@@ -0,0 +1,5 @@
+/workspaces/rust_initiation/first_project/target/debug/deps/first_project-c9e55de654c25ad7: src/main.rs
+
+/workspaces/rust_initiation/first_project/target/debug/deps/first_project-c9e55de654c25ad7.d: src/main.rs
+
+src/main.rs:
---
+++ first_project/target/debug/first_project.d
@@ -0,0 +1,1 @@
+/workspaces/rust_initiation/first_project/target/debug/first_project: /workspaces/rust_initiation/first_project/src/main.rs
